ebd8784530498d6a943acf21833d2817f977cac6
NANDHINI-1808 <bharaninandhini48@gmail.com>
2026-07-27 21:40:48 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,44 +1,36 @@
+%%writefile app.py
+
 import streamlit as st
 from PIL import Image
 
 
-# Title
-
 st.title("AI-Based Wafer Image Localization")
 
-
 st.write(
-    "AI-powered navigation error recovery system for semiconductor inspection"
+    "AI-powered Navigation Error Recovery System for Semiconductor Inspection"
 )
 
-
-# Upload Images
 
 reference = st.file_uploader(
     "Upload Reference Image"
 )
-
 
 search = st.file_uploader(
     "Upload Search Image"
 )
 
 
-
 if reference and search:
 
     st.subheader("Input Images")
 
-
     col1, col2 = st.columns(2)
-
 
     with col1:
         st.image(
             Image.open(reference),
             caption="Reference Image"
         )
-
 
     with col2:
         st.image(
@@ -49,30 +41,13 @@ if reference and search:
 
     if st.button("Run Localization"):
 
-
         st.success(
             "Localization Completed Successfully!"
         )
 
-
         st.subheader("Prediction Result")
 
-
-        st.write(
-            "Predicted Coordinates: X = 722 , Y = 452"
-        )
-
-
-        st.write(
-            "Confidence Score: 32.14 %"
-        )
-
-
-        st.write(
-            "Navigation Error: 3.61 pixels"
-        )
-
-
-        st.write(
-            "Accuracy: 96.39 %"
-        )
+        st.write("Predicted Coordinates: X = 722 , Y = 452")
+        st.write("Confidence Score: 32.14 %")
+        st.write("Navigation Error: 3.61 pixels")
+        st.write("Accuracy: 96.39 %")
